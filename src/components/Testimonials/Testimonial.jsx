96f835e1d40a576ad9e6c5fbb47579d4b3321ba0
alvo254 <alvinkhamron@gmail.con>
2022-07-28 10:34:17 +0300

    testimonials section

---
+++ src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import './Testimonials.css'
+import { testimonialsData } from '../../assets/testimonialsData'
+import RightArrow from '../../images/rightArrow.png'
+import leftArrow from '../../images/leftArrow.png'
+import {motion} from 'framer-motion'
+import axios from 'axios'
+
+
+const Testimonial = () => {
+  axios("https://phase-2-api.herokuapp.com/testimonials?")
+  // .then((data) => {data.json()})
+  .then((res) => {console.log(res.data[0])})
+
+  const HandleSubmit = (e) => {
+    e.preventDefault()
+    const formData = {
+      review: review,
+      name: name,
+      status:status
+    }
+
+  fetch("https://phase-2-api.herokuapp.com/testimonials", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body:JSON.stringify(formData)
+  })
+  
+
+  const transition = {type:'spring', duration:3}
+  const [selected, setSelected] = useState(0)
+  const tLength = testimonialsData.length
+  return (
+    <div className="testimonials">
+        <div className="left-t">
+            <span>Testimonials</span>
+            <span className='stroke-text'>What they</span>
+            <span>say about us</span>
+            <span>{testimonialsData[selected].review}</span>
+            <span style={{color:"var(--orange)"}}>{testimonialsData[selected].name} - {testimonialsData[selected].status}</span>
+        </div>
+
+        <div className="right-t">
+          <motion.div
+          initial={{opacity:0, x:-100}}
+          transition={{...transition, duration:2}}
+          whileInView={{opacity:1, x:0}}
+          >
+
+          </motion.div>
+
+          <motion.div
+                    initial={{opacity:0, x:100}}
+                    transition={{...transition, duration:2}}
+                    whileInView={{opacity:1, x:0}}
+          >
+
+          </motion.div>
+          <img src={testimonialsData[selected].image} alt="" />
+          <div className="arrows">
+            <img
+            onClick={() => {
+              selected === 0 
+            ? setSelected(tLength -1)
+            : setSelected((prev) => prev -1) }} 
+             src={leftArrow} alt="" />
+
+            <img 
+            onClick={() => {
+              selected === tLength - 1
+            ? setSelected(0)
+            : setSelected((prev) => prev + 1) }} 
+            src={RightArrow} alt="" />
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonial
