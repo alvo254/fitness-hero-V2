96bf90fba6a8de849124cc28d272527d0f3c01d1
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-08-17 15:06:34 +0300

    Update Programs.jsx

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './Programs.css'
-import {programsData} from '../../assets/programsData'
+// import {programsData} from '../../assets/programsData'
 import RightArrow from '../../images/rightArrow.png'
 import { useEffect } from 'react'
 import axios from 'axios'
