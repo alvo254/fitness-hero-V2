bd60a0fb5d653e6f45c9c722603655a5362fdaaf
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-08-17 15:05:45 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import Logo from '../../images/logo.png'
+// import Logo from '../../images/logo.png'
 import { Link } from 'react-router-dom'
 
 function Header() {
